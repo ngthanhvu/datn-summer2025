c3952147c5543767c5be772d52a0ef095e022a21
hoaily19 <hoailxpk03594@gmail.com>
2025-06-11 21:58:23 +0700

    create: database comment

---
+++ backend/app/Http/Controllers/ProductReviewController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductReview;
+use App\Models\ReviewImage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+class ProductReviewController extends Controller
+{
+    public function index(Request $request)
+    {
+        
+    }
+
+    public function store(Request $request)
+    {
+        
+    }
+
+    public function update(Request $request, ProductReview $review)
+    {
+       
+    }
+
+    public function destroy(ProductReview $review)
+    {
+        
+    }
+
+    public function approve(ProductReview $review)
+    {
+     
+    }
+
+    public function hide(ProductReview $review)
+    {
+       
+    }
+}
