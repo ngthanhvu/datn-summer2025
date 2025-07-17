9621b9987c1abf48639dbb2fd4615c496dc118bd
ngthanhvu <nguyenthanhvudvfb@gmail.com>
2025-07-17 11:20:11 +0700

    Them lay ip nguoi dung

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM Start Frontend (npm run dev)
-start "Frontend Server" cmd /k "cd frontend && npm run dev"
-
-REM Start Backend (php artisan serve)
-start "Backend Server" cmd /k "cd backend && php artisan serve"
-
-exit 
